e1bcce79aae48ad1f70025ea33061e1ee3731520
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-08-17 14:40:16 -0500

    fixed .gitignore, styling

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -8,13 +8,16 @@ class Nav extends Component {
         return ( 
             <div>
                 <nav>
+                    <div>
+                        <img src="" alt="logo"/>
+                    </div>
                     <div className="nav-links">
-                        <Link to="/home"><p>Dash</p></Link>
+                        <Link to="/home"><p>Home</p></Link>
                         <Link to="/profile"><p>Profile</p></Link>
                         <Link to="/matches"><p>Matches</p></Link>
                     </div>
                     <div className="nav-photo-div">
-                        <img src="https://via.placeholder.com/150" alt="user photo" className="nav-user-img"/>
+                        <img src="https://via.placeholder.com/150" alt="user photo"/>
                     </div>
                 </nav>
             </div>
