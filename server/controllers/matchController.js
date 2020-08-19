41edf08ad44bfe44143cb777632101aaab254296
CameronK24 <cskelly15@gmail.com>
2020-08-19 15:15:04 -0600

    started work on match controller

--- server/controllers/matchController.js
+++ server/controllers/matchController.js
@@ -1,3 +1,35 @@
 module.exports = {
-    
+    getMyMatchRequests: async (req, res) => {
+        const {id} = req.params;
+        const db = req.app.get('db');
+
+        await db.matches.get_my_match_requests([id])
+            .then(results => {
+                res.status(202).send(results);
+            })
+            .catch(err => res.status(400).send(err));
+    },
+
+    addMatchRequest: async (req, res) => {
+        const {id} = req.params;
+        const {username} = req.body;
+        const db = req.app.get('db');
+
+        await db.matches.add_match_request([id, username])
+            .then(results => {
+                res.sendStatus(202);
+            })
+            .catch(err => res.status(400).send(err));
+    },
+
+    getMyMatches: async (req, res) => {
+        const {id} = req.params;
+        const db = req.app.get('db');
+
+        await db.matches.get_my_matches([id])
+            .then(results => {
+                res.status(202).send(results);
+            })
+            .catch(err => res.status(400).send(err));
+    }
 }
