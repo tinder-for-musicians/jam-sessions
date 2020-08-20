3c353d76ff8fc5ef98edeb429bee873b9d0ae6bc
ReedDow <reed.r.dow@gmail.com>
2020-08-20 16:34:55 -0600

    Matches.js fns, Home.js checkUser

--- src/Components/Matches/Matches.js
+++ src/Components/Matches/Matches.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import "./Matches.scss";
 import { Grid, Image, Container, Card, Icon, Accordion } from 'semantic-ui-react';
 import "../icons/accordion.png";
+import axios from 'axios';
+import {connect} from 'react-redux';
+import {getUser} from '../../redux/reducer';
 
 
 class Matches extends Component {
@@ -14,20 +17,45 @@ class Matches extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            array : [1, 2, 3, 4, 5, 6]
+            array: [1, 2, 3, 4, 5, 6],
         }
     }
-    
-    
 
-  
-    render() { 
+    componentDidMount(){
+        this.getMatches();
+    }
+
+    //retrieves matches from db to display in state onMount - may need to create Match.js component to render different data in each mapped match component. 
+    getMatches = () => {
+        axios.get(`/api/match${this.props.user.user_id}`)
+            .then(res => this.setState({ array: res.data }))
+            .catch(err => console.log(err))
+    };
+
+    //on-click deletes match
+    deletePost = (id) => {
+        axios.delete(`/api/match/${id}`)
+        .then(() => {
+            this.getMatches();
+        })
+        .catch(err => console.log(err))
+    }
+
+    //adds match to db user_id foreign key
+    handleClick = (match) => {
+        console.log(this.props)
+        axios.post('/api/match', { id: this.props.user.user_id, match })
+            .catch(err => console.log(err));
+    };
+
+
+    render() {
         const mappedMatches = this.state.array.map((index => {
-           return <div>
-                    <Grid.Column>
-                        <Card key={index}>
-                            <Image src='https://via.placeholder.com/150' wrapped ui={false} />
-                            <Card.Content>
+            return <div>
+                <Grid.Column>
+                    <Card key={index}>
+                        <Image src='https://via.placeholder.com/150' wrapped ui={false} />
+                        <Card.Content>
                             <Card.Header>Matthew</Card.Header>
                             <Card.Meta>
                                 <span className=''>Proficiency: 5 yrs</span>
@@ -35,33 +63,34 @@ class Matches extends Component {
                             <Card.Description id="matches-name">
                                 Matthew is a musician living in Nashville.
                             </Card.Description>
-                            </Card.Content>
-                            <Card.Content extra>
+                        </Card.Content>
+                        <Card.Content extra>
                             <a>
                                 <Icon name='music' />
                                 Instruments
-                                <img src={require("../icons/accordion.png")} alt="banjo"/>
+                                <img src={require("../icons/accordion.png")} alt="banjo" />
                             </a>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
+                        </Card.Content>
+                    </Card>
+                </Grid.Column>
             </div>
         }))
-        return ( 
+        return (
             <div className="matches-wrapper">
                 <div className="mapped-matches">
                     <Container>
                         <Grid>
                             <Grid.Row columns={6}>
-                            {mappedMatches}
+                                {mappedMatches}
                             </Grid.Row>
 
                         </Grid>
                     </Container>
                 </div>
             </div>
-         );
+        );
     }
 }
- 
-export default Matches;
+
+const mapStateToProps = reduxState => reduxState;
+export default connect(mapStateToProps, {getUser})(Matches);
