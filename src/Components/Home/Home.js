edcdd0fe2411e6290236426836e1aacaa791b3c2
ReedDow <reed.r.dow@gmail.com>
2020-08-22 18:03:20 -0600

    Home search fn and dropdowns

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,21 +4,22 @@ import Search from "./Search";
 import { connect } from 'react-redux';
 import { getProfile } from '../../redux/reducer';
 import { ToastsContainer, ToastsStore } from 'react-toasts';
+import { Dropdown } from 'semantic-ui-react'
 import axios from 'axios';
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            instruments: [],
-            level: ['hobbyist', 'beginner', 'intermediate', 'advanced'],
-            yearsExp: [1, 3, 5, 10],
-            location: [5, 10, 20],
-            search: '',
-            filteredInstruments: [],
-            dropdownView1: false,
-            dropdownView2: false,
-            dropdownView3: false,
+            instrument: [],
+            level: [],
+            experience: [],
+            distance: [],
+            setInstrument: '',
+            setExperience: '',
+            setLevel: '',
+            setDistance: '',
+            filteredMatches: [],
         }
     }
 
@@ -28,121 +29,189 @@ class Home extends Component {
             .then()
             .catch(() => this.props.history.push('/'));
 
-        this.getProfileInfo();
-        
+        // this.getProfileInfo();
+        this.getMatchesInfo()
         this.getInstruments();
+
     }
 
     toggleDropdown1 = () => {
-        this.setState({dropdownView1: !this.state.dropdownView1})
+        this.setState({ dropdownView1: !this.state.dropdownView1 })
     }
     toggleDropdown2 = () => {
-        this.setState({dropdownView2: !this.state.dropdownView2})
+        this.setState({ dropdownView2: !this.state.dropdownView2 })
     }
     toggleDropdown3 = () => {
-        this.setState({dropdownView3: !this.state.dropdownView3})
+        this.setState({ dropdownView3: !this.state.dropdownView3 })
     }
 
-    getProfileInfo = () => {
-        axios.get(`/api/profile`)
+    getMatchesInfo = () => {
+        axios.get(`/api/search`)
             .then(res => {
-                this.props.getProfile(res.data);
+                this.setState({
+                    filteredMatches: res.data
+                })
             })
     }
 
-    handleSearch = (e) => {
-        let keyword = e.target.value;
-        this.setState({ search: keyword })
+    handleSearch = () => {
+        const { setInstrument, setLevel, setExperience, setDistance } = this.state
+        axios.post('api/search', { instrument: setInstrument, level: setLevel, experience: setExperience, distance: setDistance })
+            .then(res => {
+                this.setState({
+                    filteredMatches: res.data
+                })
+            })
     };
 
-    // handleClick = (match) => {
-    //     axios.post('/api/match')
+    handleSetInstrument = (e, {value}) => {
+        this.setState({ setInstrument: value })
+        console.log(this.state.setInstrument)
+    }
+
+    handleSetLevel = (e, {value}) => {
+        this.setState({ setLevel: value })
+        console.log(this.state.setLevel)
+    }
+
+    handleSetExperience = (e, {value}) => {
+        this.setState({ setExperience: value })
+        console.log(this.state.setExperience)
+    }
+
+
+    // handleClick = () => {
+    //     axios.post('/api/search')
     // }
 
-    handleReset = () =>{
-        this.setState({search: ''})
-        this.setState({filteredInstruments: []})
-        this.setState({yearsExp: []})
-        this.setState({level: []})
-        this.setState({location: []})
-      };
+    handleReset = () => {
+        
+        this.setState({ setExperience: '' })
+        this.setState({ setLevel: ''})
+        this.setState({ setDistance: '' })
+        this.setState({ setInstrument: '' })
+    };
 
     getInstruments = () => {
-        axios.get('api/search')
-            .then(res => this.setState({ instruments: res.data.user_instruments[0], level: res.data.user_instruments[1], yearsExp: res.data.user_instruments[2]
-             }))
-            // console.log(this.state.instruments)
+        axios.get('api/instruments')
+            .then(res => {
+                // console.log(res.data)
+                this.setState({
+                    instrument: res.data.instruments,
+                    level: res.data.levels,
+                    experience: res.data.experience_years
+                })
+
+            }
+            )
             .catch(err => console.log(err))
     }
 
     render() {
-        let filteredByInstruments = this.state.instruments.filter((data) => {
-            if (this.state.search == '')
-                return false
-            else if (data.toLowerCase().includes(this.state.search.toLowerCase())) {
-                return true
-            }
-            else return false
-        }).map((data) => {
-            return (
-                <div>
-                    <span
-                        onClick={() => {
-                            this.handleClick(data);
-                            ToastsStore.success("Matched!")
-                        }}
-                        className='tooltip'>
-                        <ToastsContainer store={ToastsStore} />
+        const { instrument, level, distance, experience } = this.state;
+        const mappedMatches = this.state.filteredMatches
+            .map((match) => {
+                const { user_id, username, profile_pic, bio, distance_of_match } = match
+                console.log(match)
+                return (
+                    <div key={user_id}>
+                        <span
+                            onClick={() => {
+                                this.handleClick(match);
+                                ToastsStore.success("Matched!")
+                            }}
+                            className='tooltip'>
+                            <ToastsContainer store={ToastsStore} />
 
-                        {data}
-                    </span>
 
-                </div>
+                        </span>
+                        <p>{username}</p>
 
+                        <p>{bio}</p>
+
+                    </div>
+                )
+
+
+            })
+        console.log(instrument)
+        const instrumentOptions = []
+        instrument.forEach(instrument => {
+            console.log(instrument)
+            instrumentOptions.push(
+                {
+                    key: instrument,
+                    text: instrument,
+                    value: instrument,
+
+                }
             )
         })
+        const levelOptions = [{
+            key: 'select level',
+            text: 'select level',
+            value: '',
+        }]
+        level.forEach(level => {
+            console.log(level)
+            levelOptions.push(
+                {
+                    key: level,
+                    text: level,
+                    value: level,
 
+                }
+            )
+        })
+        const experienceOptions = []
+        experience.forEach(experience => {
+            console.log(experience)
+            experienceOptions.push(
+                {
+                    key: experience,
+                    text: experience,
+                    value: experience,
+
+                }
+            )
+        })
+        console.log(instrumentOptions)
+        console.log(levelOptions)
+        console.log(experienceOptions)
         return (
             <div className=
                 'home-body'>
-                <p className='intro'> Search Profiles by Instrument, Skill Level, Years of Experience and Location</p>
+               <p className = 'intro'>Search people to jam with</p>
                 <section className='searchcontainer'>
-                    <input
-                        type='text'
-                        className='searchinput'
-                        placeholder='Search by instrument'
-                        // onChange={(e) => this.handleSearch(e)}  
-                        //uncomment ^ to search as user types 
+                    <Dropdown
+                        placeholder='Select Instrument'
+                        fluid
+                        onChange={this.handleSetInstrument}
+                        search
+                        selection
+                        options={instrumentOptions}
                     />
-                    
-                    <div className = 'dropdown1' onClick = {this.toggleDropdown1}>Skill Level</div>
-                    {this.state.dropdownView1 ? (
-                        <div classname = 'display1'>
-                            <ul>
-                            {this.state.level}
-                            </ul>
-                        </div>
-                    )
-                    : null}
-                    <div className = 'dropdown2' onClick = {this.toggleDropdown2}> Years of Experience |v</div>
-                    {this.state.dropdownView2 ? (
-                        <nav>
-                            <ul>
-                            {this.state.yearsExp}
-                            </ul>
-                        </nav>
-                    )
-                    : null}
-                    <div className = 'dropdown3' onClick = {this.toggleDropdown3}>Location</div>
-                    {this.state.dropdownView3 ? (
-                        <nav>
-                            <ul>
-                            {this.state.location}
-                            </ul>
-                        </nav>
-                    )
-                    : null}
-                    <button onClick={(e) => this.handleSearch(e)}
+
+                    <Dropdown
+                        placeholder='Select Level'
+                        fluid
+                        onChange={this.handleSetLevel}
+                        search
+                        selection
+                        options={levelOptions}
+                    />
+
+                    <Dropdown
+                        placeholder='Select Experience'
+                        fluid
+                        onChange={this.handleSetExperience}
+                        search
+                        selection
+                        options={experienceOptions}
+                    />
+
+
+                    <button onClick={this.handleSearch}
                         className='srchbtn'>Search</button>
 
                     <button
@@ -152,7 +221,7 @@ class Home extends Component {
                 </section>
 
                 <section className='dashboard-profile-view'>
-                    {filteredByInstruments}
+                    {mappedMatches}
                 </section>
 
             </div>
