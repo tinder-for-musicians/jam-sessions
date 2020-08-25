8e0dbb83cd5e4f91074a681fb726932971ce6da0
CameronK24 <cskelly15@gmail.com>
2020-08-25 16:16:31 -0600

    preparing for master merg

--- src/Components/Matches/chat.js
+++ src/Components/Matches/chat.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect, useRef} from 'react';
 import socket from 'socket.io-client';
 import {connect} from 'react-redux';
 
-const client = socket(process.env.REACT_APP_IO_PORT);
-
 const Chat = props => {
+    const [client, setClient] = useState(() => {
+        const initialState = socket(process.env.REACT_APP_IO_PORT);
+        return initialState;
+    });
     const [chatMessage, setChatMessage] = useState('');
     const [newMessage, setNewMessage] = useState('');
     const [listMessages, setListMessages] = useState([]);
@@ -12,21 +14,21 @@ const Chat = props => {
     // const [username, setUsername] = useState('')
 
     useEffect(() => {
-        const abortController = new AbortController();
-        async function getNewMessage() {
-            await client.on('newMessage', msg => {
-                console.log(msg);
-                setNewMessage(msg);
-            });
-            return function cleanup() {
-                abortController.abort();
+        if (client) {
+            const abortController = new AbortController();
+            async function getNewMessage() {
+                await client.on('newMessage', msg => {
+                    setNewMessage(msg);
+                });
+                return function cleanup() {
+                    abortController.abort();
+                }
             }
+            getNewMessage();
         }
-        getNewMessage();
     }, []);
 
     useEffect(() => {
-        // console.log(newMessage, listMessages);
         setListMessages([...listMessages, newMessage]);
     }, [newMessage]);
 
@@ -41,20 +43,20 @@ const Chat = props => {
     const send = async () => {
         const combinedMessage = `${props.user.username}: ${chatMessage}`
         await client.emit('chatMessage', combinedMessage);
-        // console.log(username);
         setChatMessage('');
     }
 
     return (
-        <div className='chat-display'>
-            <ul className='messages'>
-                {mappedMessages}
-            </ul>
-            <section className='create-message-section'>
-                <input className='message-input' value={chatMessage} onChange={e => setChatMessage(e.target.value)} />
-                <button onClick={send}>Send</button>
-            </section>
-        </div>
+            <div className='chat-display'>
+                <ul className='messages'>
+                    {mappedMessages}
+                    <li></li>
+                </ul>
+                <section className='create-message-section'>
+                    <input className='message-input' value={chatMessage} onChange={e => setChatMessage(e.target.value)} />
+                    <button onClick={send}>Send</button>
+                </section>
+            </div>
     )
 }
 
