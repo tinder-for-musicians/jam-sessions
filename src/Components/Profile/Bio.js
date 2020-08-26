9a38a694c3a1a99f6e849cdb1e833c9e5551c5fa
ReedDow <reed.r.dow@gmail.com>
2020-08-26 16:43:59 -0600

    profile

--- src/Components/Profile/Bio.js
+++ src/Components/Profile/Bio.js
@@ -8,21 +8,32 @@ class Bio extends Component {
   constructor(props){
   super(props)
   this.state={
-    activeItem: ''
+    activeItem: '',
+    instruments: []
   }
   }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   handleActiveItem = () => {
+    console.log(this.props.user)
     if (this.state.activeItem === 'bio') {
+      console.log(this.props.user)
       return <p>{this.props.user.bio}</p>;
-    } else if (this.state.activeItem === 'instruments') {
+    } else if (this.state.activeItem === 'instrument') {
       return <p>{this.props.user.user_instruments}</p>;
     } else {
       return ("Nothing works");
     }
   }
+
+  // mappedInstruments = () => {
+  //   this.state.instruments = this.props.user.user_instruments.map( elem => {
+  //     console.log(elem[1])
+  //     return elem[1];
+      
+  //   })
+  // }
 
   render() {
     const { activeItem } = this.state
@@ -30,32 +41,33 @@ class Bio extends Component {
       <Grid>
         <Grid.Column width={4}>
           <Menu fluid vertical tabular>
-            <Menu.Item
+            {/* <Menu.Item
               name='bio'
               active={activeItem === 'bio'}
               onClick={this.handleItemClick}
-            />
+            /> */}
             <Menu.Item
-              name='instruments'
-              active={activeItem === 'instruments'}
+              name='instrument'
+              active={activeItem === 'instrument'}
               onClick={this.handleItemClick}
             />
-            <Menu.Item
+            {/* <Menu.Item
               name='companies'
               active={activeItem === 'companies'}
               onClick={this.handleItemClick}
-            />
-            <Menu.Item
+            /> */}
+            {/* <Menu.Item
               name='links'
               active={activeItem === 'links'}
               onClick={this.handleItemClick}
-            />
+            /> */}
           </Menu>
         </Grid.Column>
 
         <Grid.Column stretched width={12}>
           <Segment>
             {this.handleActiveItem()}
+            {/* {this.mappedInstruments()} */}
           </Segment>
         </Grid.Column>
       </Grid>
